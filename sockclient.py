15e1b22f445573041d920c527c7e4c72b44d9f09
Noah Gary <goahnary@gmail.com>
2016-04-02 17:42:30 -0500

    added loop to send text time

--- sockclient.py
+++ sockclient.py
@@ -16,14 +16,20 @@ while(1) :
      
     try :
         #Set the whole string
-        s.sendto(msg, (host, port))
+        #s.sendto(msg, (host, port))
+
+        with open("./test.txt", "rb") as fi:
+            buf = fi.read(32)
+            while (buf):
+               s.sendto(buf, (host, port))
+               buf = fi.read(32)
          
-        # receive data from client (data, addr)
+        # receive data from server (data, addr)
         d = s.recvfrom(1024)
         reply = d[0]
         addr = d[1]
          
-        print 'Server reply : ' + reply
+        print 'Server() reply : ' + reply
      
     except socket.error, msg:
         print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
--- sockserver.py
+++ sockserver.py
@@ -30,7 +30,7 @@ while 1:
     data = d[0]
     addr = d[1]
      
-    if not data: 
+    if not data:
         break
      
     reply = 'OK...' + data
