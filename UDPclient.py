c9fa94ff6c3730a62c1a53f0b7dd978331d0b889
skywalker45 <danielskywalker45@yahoo.com>
2016-04-02 16:38:49 -0400

    Added files via upload

---
+++ UDPclient.py
@@ -0,0 +1,17 @@
+import socket
+import sys
+
+HOST = raw_input("Enter an IP to connect to\n")
+PORT = 9999
+data = raw_input("Enter a message to send\n").join(sys.argv[1:])
+
+# SOCK_DGRAM is the socket type to use for UDP sockets
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# As you can see, there is no connect() call; UDP has no connections.
+# Instead, data is directly sent to the recipient via sendto().
+sock.sendto(data + "\n", (HOST, PORT))
+received = sock.recv(1024)
+
+print "Sent:     {}".format(data)
+print "Received: {}".format(received)    
