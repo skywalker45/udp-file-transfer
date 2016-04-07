d7de32f0693929cedf8512f916676eb0bbcf6b79
Noah Gary <goahnary@gmail.com>
2016-04-06 21:05:18 -0500

    server is sending acknologements now

--- sockclient.py
+++ sockclient.py
@@ -2,13 +2,14 @@ import socket   #for sockets
 import sys  #for exit
 import threading
 import select
+import time
 
 # create dgram udp socket
 try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 except socket.error:
-    print 'Failed to create socket'
-    sys.exit()
+	print 'Failed to create socket'
+	sys.exit()
 
 host = raw_input('Enter an IP to connect to : ')
 port = 8888;
@@ -18,86 +19,86 @@ filename = ""
 packetNum = -1
 packets = []
 tracking = []   # this variable will keep track of the time that
-                # has passed since each packet left. When that time
-                # expires it will be sent again.
+				# has passed since each packet left. When that time
+				# expires it will be sent again.
 
 def main():
 
-    global filename
-    filename = raw_input('Enter name of File: ')
+	global filename
+	filename = raw_input('Enter name of File: ')
 
-    #http://stackoverflow.com/questions/25465792/python-binary-eof
-    with open(filename, "rb") as infile:
-        while True:
-            data = infile.read(32)
-            if not data:
-                break
+	#http://stackoverflow.com/questions/25465792/python-binary-eof
+	with open(filename, "rb") as infile:
+		while True:
+			data = infile.read(32)
+			if not data:
+				break
 
-            package(data)
-        print "[BREAK]"
-        sendPackets()
-        startTracking()
+			package(data)
+		print "[BREAK]"
+		sendPackets()
+		startTracking()
 
 def package(d):
-    print "packaging data:\n\"\n" + d + "\n\""
+	global packetNum
+	packetNum += 1
 
-    global packetNum
-    packetNum += 1
-
-    packets.append((packetNum,filename, d))
-    index = len(packets)
-    trackit(index)
+	packets.append((packetNum,filename, d))
+	index = len(packets)
+	trackit(index)
 
 def trackit(num):
-    print "tracking packet " + str(num)
-    tracking.append(256)
+	print "tracking packet " + str(num)
+	tracking.append(256)
 
 def sendPackets():
-    for packet in packets:
-        packet = packet + (len(packets),)
-        s.sendto(str(packet), (host, port))
+	for packet in packets:
+		packet = packet + (len(packets),)       # adding total number of packets to packet.
+												# This could not be calculated in advance
+												# and is guarunteed to be correct if calulated now.
+		s.sendto(str(packet), (host, port))
 
 def startTracking():
-    #decrement each array by one and resend the corresponding packet when the count reaches 0
-    print "startTracking"
-    # while 1:
-    #     tracking[:] = [x - 1 for x in tracking]
-    #     print "decrementing: " + str(tracking[0])
-    #     if tracking[0] <= 0:
-    #         break
+	#decrement each array by one and resend the corresponding packet when the count reaches 0
+	print "startTracking"
+	# while 1:
+	#     tracking[:] = [x - 1 for x in tracking]
+	#     print "decrementing: " + str(tracking[0])
+	#     if tracking[0] <= 0:
+	#         break
 
 class listen(threading.Thread):
-    
-    def __init__(self):
-        print "Starting listen() thread..."
-        threading.Thread.__init__(self)
-        self.running = 1
+	
+	def __init__(self):
+		print "Starting listen() thread..."
+		threading.Thread.__init__(self)
+		self.running = 1
 
-    def run(self):
-        while self.running:
-            ready = select.select([s], [], [], 1)
-            if ready[0]:
-                d = s.recvfrom(1024)
-                reply = d[0]
-                addr = d[1]
-                
-                print reply
+	def run(self):
+		while self.running:
+			ready = select.select([s], [], [], 1)
+			if ready[0]:
+				d = s.recvfrom(1024)
+				reply = d[0]
+				addr = d[1]
 
-        print "...listen() stopped."
+				print reply
 
-    def kill(self):
-        self.running = 0
-        print "Killing listen()..."
+				if reply == "":
+					self.kill()
+
+		print "...listen() stopped."
+
+	def kill(self):
+		self.running = 0
+		print "Killing listen()..."
 
 
 if __name__ == '__main__':
-    # listen for response from server
-    print "doing stuff"
-    # t = threading.Thread(target=listen)
-    listener = listen()
-    listener.start()
-    print "running main..."
-    main()
-    print packets
-    print tracking
-    listener.kill()
+
+	listener = listen()
+	listener.start()            # listen for response from server
+	
+	main()
+	time.sleep(5)
+	listener.kill()             # kill listener in case it's still running
--- sockserver.py
+++ sockserver.py
@@ -9,20 +9,20 @@ PORT = 8888 # Arbitrary non-privileged port
  
 # Datagram (udp) socket
 try :
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print 'Socket created'
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	print 'Socket created'
 except socket.error, msg :
-    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
+	print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+	sys.exit()
  
  
 # Bind socket to local host and port
 try:
-    s.bind((HOST, PORT))
+	s.bind((HOST, PORT))
 except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
-     
+	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+	sys.exit()
+	 
 print 'Socket bind complete'
  
 #now keep talking with the client
@@ -32,16 +32,16 @@ datacoming = False
 
 while 1:
 
-    # receive data from client (data, addr)
-    d = s.recvfrom(1024)
-    data = d[0]
-    addr = d[1]
+	# receive data from client (data, addr)
+	d = s.recvfrom(1024)
+	data = d[0]
+	addr = d[1]
 
-    if not data:
-        break
+	if not data:
+		break
 
-    s.sendto(data, addr)
-     
-    print data.strip()
-     
+	packet = ast.literal_eval(data)
+	s.sendto(str(packet[0]), addr)
+	print packet
+	 
 s.close()
